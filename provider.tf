03d23808e69b3deaa138edf8f86f63912a768598
jozefowicz_adam <jozefowicz.adam@gmail.com>
2024-11-22 16:24:37 +0000

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region  = "us-east-1"
+  # profile = "panda" # tylko gdy istnieje taki --profile w ~/.aws/credentials
+}
+
+ terraform
+
+terraform {
+  backend "s3" {
+    bucket = "adam-jozefowicz-panda-devops-core-19"
+    key = "infra/terraform.state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+     version = "3.33"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
